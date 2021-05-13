362f54d539a9d092e640fb41c1053e4c8a217245
AditriSharma <75737419+AditriSharma@users.noreply.github.com>
2021-05-13 19:44:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var bigBall;
+
+function setup(){
+    createCanvas(500,500); 
+
+   database=firebase.database()
+   console.log(database)
+
+    bigBall = createSprite(250,250,10,10);
+    bigBall.shapeColor = "red";
+
+    var bigBallPosition=database.ref("ball/position")
+    bigBallPosition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+ database.ref("ball/position").set({
+     x:position.x+x,
+     y:position.y+y
+ })
+
+}
+
+
+function readPosition(data) {
+
+ position=data.val()
+ console.log(position.x)
+ bigBall.x=position.x
+ bigBall.y=position.y
+}
+
+
+function showError() {
+    console.log("error")
+}
